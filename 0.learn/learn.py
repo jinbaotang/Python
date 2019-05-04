624707156db673184088e7fb5de36397c0b2a02e
jinbao.tang <jinbao.tang@foxmail.com>
2019-05-04 23:39:46 +0800

    学完切片

--- 0.learn/learn.py
+++ 0.learn/learn.py
@@ -1,5 +1,15 @@
-def power(x, n=3):
-    return  x**n
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = n + sum
+    return  sum
 
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
 
-print(power(2, 5))
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+
+d = {'a': 1, 'b': 2, 'c': 3}
+for k, v in d.items():
+    print(k,v)
+
